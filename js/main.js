03ba66271c33fe700b78c424a43941981c289d1a
Nick Underwood <nunderwood6@gmail.com>
2019-02-05 14:57:25 -0600

    Debugged script and added comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //initialize function called when the script loads
 function initialize(){
-    cities();
+   //store cityPop so addColumns can reference
+    var cityData = cities();
+    addColumns(cityData);
+    addEvents();
 };
 
 //function to create a table with cities and their populations
@@ -45,7 +48,82 @@ function cities(){
         //add the row's html string to the table
         $("table").append(rowHtml);
     };
+
+    return cityPop;
+
 };
+
+///start pasted debug.js code
+function addColumns(cityPop){
+    console.log(cityPop);
+    //for each row add city size column
+    $('tr').each(function(i){
+
+    //if top row add header
+        if (i == 0){
+
+            $(this).append('<th>City Size</th>');
+        } 
+        //otherwise add appropriate citysize based on cityPop column
+        else {
+
+            var citySize;
+
+            //remove comma and convert to number for comparison
+            if (parseFloat(cityPop[i-1].population.replace(/,/g, "")) < 100000){
+                citySize = 'Small';
+
+            } else if (parseFloat(cityPop[i-1].population.replace(/,/g, "")) < 500000){
+                citySize = 'Medium';
+
+            } else {
+                citySize = 'Large';
+            };
+
+            $(this).append('<td>' + citySize + '</td>');
+        };
+    });
+};
+//add user triggered events
+function addEvents(){
+
+    //change to random color on mouseover
+    $('table').on("mouseover", function(){
+
+        var color = "rgb(";
+
+        //random number for each value of rgb color
+        for (var i=0; i<3; i++){
+
+            var random = Math.round(Math.random() * 255);
+
+            color += random;
+
+            //format correctly for css
+            if (i<2){
+                color += ",";
+            
+            } else {
+                color += ")";
+            }
+
+            //change color
+        $(this).css('color', color);
+
+        }
+
+    });
+
+//alert when table is clicked
+    function clickme(){
+        alert('Hey, you clicked me!');
+    };
+
+    $('table').on('click', clickme);
+};
+
+
+
 
 //call the initialize function when the window has loaded
 $(document).ready(initialize);
